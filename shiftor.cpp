27773892394e9235c1ea160426a6ce7449041bfc
Gabriela Araujo Britto <gab@ug4c31.windows.cin.ufpe.br>
2017-09-12 13:51:05 -0300

    shift or compilou falta testar

--- shiftor.cpp
+++ shiftor.cpp
@@ -5,22 +5,19 @@ using namespace std;
 
 typedef unsigned int uint;
 const int word_size = 32;
+const uint one = 1U;
 
 struct bitvector {
-	int size;
+	int size; //number of bits in the bitvector
 	vector<uint> bitvec;
 
-	bitvector(int n) : size(n), arr((n + word_size - 1) / word_size, 0) {}
+	bitvector(int n, uint b = 0) : size(n), bitvec((n + word_size - 1) / word_size, b) {}
 	
-	int size() {
-		return size;
-	}
-
 	void set(int pos) {
-		if(pos < size && pos > 0) {
+		if(pos < size && pos >= 0) {
 			int vector_idx = pos / word_size;
-			int bit_idx = pos - vector_idx * word_size;
-			bitvec[vector_idx] |= 1U << bit_idx;
+			int bit_idx = pos % word_size;
+			bitvec[vector_idx] |= one << bit_idx;
 		}
 		else {
 			cerr << "pos out of bounds" << endl;
@@ -28,10 +25,10 @@ struct bitvector {
 	}
 
 	void reset(int pos) {
-		if(pos < size && pos > 0) {
+		if(pos < size && pos >= 0) {
 			int vector_idx = pos / word_size;
-			int bit_idx = pos - vector_idx * word_size;
-			bitvec[vector_idx] &= ~(1U << bit_idx);
+			int bit_idx = pos % word_size;
+			bitvec[vector_idx] &= ~(one << bit_idx);
 		}
 		else {
 			cerr << "pos out of bounds" << endl;
@@ -39,13 +36,14 @@ struct bitvector {
 	}
 
 	int getbit(int pos) {
-		if(pos < size && pos > 0) {
+		if(pos < size && pos >= 0) {
 			int vector_idx = pos / word_size;
-			int bit_idx = pos - vector_idx * word_size;
-			return (bitvec[vector_idx] >> bit_idx) & 1;
+			int bit_idx = pos % word_size;
+			return (bitvec[vector_idx] >> bit_idx) & one;
 		}
 		else {
 			cerr << "pos out of bounds" << endl;
+			return -1;
 		}
 	}
 
@@ -56,7 +54,7 @@ struct bitvector {
 		return *this;
 	}
 
-	bitvector& operator <<(int n) {
+	bitvector& operator <<=(int n) {
 		for(int i = size - 1; i >= 0; --i) {
 			int j = i - n;
 			if(j < 0) {
@@ -73,10 +71,64 @@ struct bitvector {
 		return *this;
 	}
 
+	bitvector& operator ++() {
+		for(int i = bitvec.size() - 1; i > 0; --i) {
+			bitvec[i] <<= 1;
+			bitvec[i] |= (bitvec[i - 1] >> (word_size - 1)) & one;
+		}
+		if(bitvec.size() > 0) {
+			bitvec[0] <<= 1;
+		}
+		return *this;
+	}
+	
+	void print() {
+		for(int i = size - 1; i >= 0; --i) {
+			printf("%2d ", i);
+		}
+		cout << endl;
+		for(int i = size - 1; i >= 0; --i) {
+			printf("%2d ", getbit(i));
+		}
+		cout << endl;
+	}
+};
+
+const int sigma = 256;
+
+vector<bitvector> char_mask(string pat) {
+	int m = pat.size();
+	vector<bitvector> C(256, bitvector(m, ~0));
+	for(int i = 0; i < m; ++i) {
+		C[pat[i]].reset(i);
+	}
+	return C;
 }
 
+vector<int> shift_or(string txt, string pat) {
+	int n = txt.size(), m = pat.size();
+	bitvector S(m, ~(0));
+	vector<bitvector> C = char_mask(pat); 
+	vector<int> occ;
+	for(int i = 0; i < n; ++i) {
+		++S |= C[txt[i]];
+		if(S.getbit(m - 1) == 0) {
+			occ.push_back(i - m + 1);
+		} 
+	}
+	return occ;
+}
 
 int main() {
-	
+	string txt, pat;
+	cin >> txt >> pat;
+	vector<int> occ = shift_or(txt, pat);
+	for(auto p: occ)
+		cout << p << " " << endl;
 	return 0;
 }
+
+
+
+
+
