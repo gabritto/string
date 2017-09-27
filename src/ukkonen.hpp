b71dd4626841f22222854b85d1fe065ba13efa27
gab <gab@cin.ufpe.br>
2017-09-26 21:27:01 -0300

    ukkonen falta testar

---
+++ src/ukkonen.hpp
@@ -0,0 +1,20 @@
+#ifndef UKKONEN_H
+#define UKKONEN_H
+
+#include "util.hpp"
+
+#include <vector>
+#include <string>
+#include <queue>
+#include <tuple>
+
+using namespace std;
+
+namespace ukkonen {
+
+void build(const string &pat, int r);
+int search(const string &txt);
+
+} //namespace ukkonen
+
+#endif
