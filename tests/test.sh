4c5856935c82d2887251e16b9530f751789123a8
gab <gab@cin.ufpe.br>
2017-10-12 16:05:54 -0300

    script de teste de exact matching

--- tests/test.sh
+++ tests/test.sh
@@ -9,7 +9,7 @@ while IFS='' read -r LINE || [[ -n "$LINE" ]]; do
   LEN=${#LINE}
   if [ $LEN -gt 0 ]; then
     for i in `seq 1 $RUNS`; do
-      (time ./../src/main -c -a $ALG LINE $TXT) 2>> $ALG$LEN.txt
+      (/usr/bin/time -f "%U %S" ./../src/main -c -a $ALG LINE $TXT) 2>> $ALG$LEN.txt
     done
   fi
 done < "$PAT"
